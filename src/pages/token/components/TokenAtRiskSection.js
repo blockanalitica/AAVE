02e42707735f8fb1a7357f994f31d89664a43835
0x3n1gm4 <0x3n1gm4@proton.me>
2022-09-12 17:36:59 +0200

    modal for risk desc

--- src/pages/token/components/TokenAtRiskSection.js
+++ src/pages/token/components/TokenAtRiskSection.js
@@ -1,7 +1,11 @@
-import { faChartBar, faChartArea } from "@fortawesome/free-solid-svg-icons";
+import {
+  faChartBar,
+  faChartArea,
+  faInfoCircle,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
-import { Col, Row } from "reactstrap";
+import { Modal, ModalHeader, ModalBody, Col, Row } from "reactstrap";
 import IconTabs from "../../../components/Tabs/IconTabs.js";
 import { withErrorBoundary } from "../../../hoc.js";
 import TokenAtRiskChart from "./TokenAtRiskChart.js";
@@ -9,6 +13,9 @@ import TimeSwitch from "../../../components/TimeSwitch/TimeSwitch.js";
 
 function TokenAtRiskSection(props) {
   const { slug, isTokenCurrencyTotal } = props;
+
+  const [modalOpen, setModalOpen] = useState(false);
+  const toggleModalOpen = () => setModalOpen(!modalOpen);
 
   const [drop, setDrop] = useState(80);
 
@@ -25,51 +32,121 @@ function TokenAtRiskSection(props) {
   description = `Simulation of markets price drop (all assets fall for x% at the same time) and ${slug} is always used as collateral to liquidate. When wallet reach health rate under 1, 50% (or max 5M) of debt position is liquidated.`;
 
   return (
-    <div>
-      <Row>
-        <Col xl={12}>
-          <h4>{slug} at risk</h4>
-          <p className="gray">{description}</p>
-          <div className="d-flex flex-direction-row justify-content-end align-items-center">
-            <TimeSwitch
-              className="mb-3 justify-content-end"
-              label="drop:"
-              activeOption={drop}
-              onChange={setDrop}
-              options={dropOptions}
-            />
-          </div>
+    <>
+      <div>
+        <Row>
+          <Col xl={12}>
+            <div className="d-flex flex-direction-row justify-content-left align-items-center">
+              <h4 className="mr-4">{slug} at risk </h4>
+              <span role="button" className="link-primary" onClick={toggleModalOpen}>
+                <FontAwesomeIcon icon={faInfoCircle} />
+              </span>
+            </div>
 
-          <IconTabs
-            tabs={[
-              {
-                title: <FontAwesomeIcon icon={faChartArea} />,
-                content: (
-                  <TokenAtRiskChart
-                    slug={slug}
-                    drop={drop}
-                    chartType="line"
-                    isTokenCurrencyTotal={isTokenCurrencyTotal}
-                  />
-                ),
-              },
-              {
-                title: <FontAwesomeIcon icon={faChartBar} />,
-                content: (
-                  <TokenAtRiskChart
-                    slug={slug}
-                    drop={drop}
-                    chartType="bar"
-                    isTokenCurrencyTotal={isTokenCurrencyTotal}
-                  />
-                ),
-              },
-            ]}
-            label="charts:"
-          />
-        </Col>
-      </Row>
-    </div>
+            <p className="gray">{description}</p>
+            <div className="d-flex flex-direction-row justify-content-end align-items-center">
+              <TimeSwitch
+                className="mb-3 justify-content-end"
+                label="drop:"
+                activeOption={drop}
+                onChange={setDrop}
+                options={dropOptions}
+              />
+            </div>
+
+            <IconTabs
+              tabs={[
+                {
+                  title: <FontAwesomeIcon icon={faChartArea} />,
+                  content: (
+                    <TokenAtRiskChart
+                      slug={slug}
+                      drop={drop}
+                      chartType="line"
+                      isTokenCurrencyTotal={isTokenCurrencyTotal}
+                    />
+                  ),
+                },
+                {
+                  title: <FontAwesomeIcon icon={faChartBar} />,
+                  content: (
+                    <TokenAtRiskChart
+                      slug={slug}
+                      drop={drop}
+                      chartType="bar"
+                      isTokenCurrencyTotal={isTokenCurrencyTotal}
+                    />
+                  ),
+                },
+              ]}
+              label="charts:"
+            />
+          </Col>
+        </Row>
+      </div>
+      <Modal isOpen={modalOpen} toggle={toggleModalOpen}>
+        <ModalHeader toggle={toggleModalOpen}>Wallet Protection Score</ModalHeader>
+        <ModalBody>
+          <p>
+            For Aave wallets, we look at multiple facets of a wallet position, both
+            current state and historical behavior:
+            <ul>
+              <li>
+                price drop percentage in volatile assets a wallet could experience
+                without any proactive health factor increase -{" "}
+                <strong>liquidatable price drop percentage</strong>
+              </li>
+              <li>
+                wallet's historical protections. An individual protection is defined as
+                a wallet's occurrence where the wallet owner prevented a liquidation by
+                increasing their wallet’s health factor (deposit, repay, etc.) -{" "}
+                <strong>number of historical liquidation protections</strong>
+              </li>
+              <li>
+                number of transactions in the last 7 days -{" "}
+                <strong>number of 7 day transactions</strong>
+              </li>
+              <li>
+                for pure stETH recursive positions (only stETH in supply and ETH in
+                borrow) we compute the additional stETH:ETH depeg percentage that a
+                wallet could experience without any proactive increase in its health
+                factor - <strong>additional stETH discount percentage buffer</strong>
+              </li>
+              <li>
+                <strong>number of historical liquidations</strong>
+              </li>
+            </ul>
+          </p>
+
+          <p>
+            <strong>Low risk</strong> wallets are defined as:
+            <ul>
+              <li>(liquidatable price drop percentage >= 50%</li>
+              <li>OR number of historical liquidation protections >= 5</li>
+              <li>OR number of 7 day transactions >= 10)</li>
+              <li>AND no historical liquidations</li>
+            </ul>
+          </p>
+          <p>
+            If the conditions for a low risk wallet are not met, the wallet can then
+            either be scored as Medium risk or High risk.
+          </p>
+          <p>
+            <strong>Medium risk</strong> wallets are defined as:
+            <ul>
+              <li>(liquidatable price drop percentage >= 30%</li>
+              <li>OR number of historical liquidation protections >= 3</li>
+              <li>OR number of 7 day transactions >= 5)</li>
+              <li>AND no historical liquidations</li>
+            </ul>
+          </p>
+          <p>
+            If the conditions for neither a Low risk wallet nor a Medium risk wallet are
+            met, the wallet is scored as <strong>High risk</strong> wallet.
+          </p>
+        </ModalBody>
+      </Modal>
+    </>
   );
 }
 
